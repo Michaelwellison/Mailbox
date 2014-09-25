38d274eee0134027c1482c701c93cf3496feb92f
Michael W. Ellison <michaelwellison@gmail.com>
2014-09-24 23:05:30 -0700

    first commit

---
+++ Mailbox-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mailbox-Swift
+//
+//  Created by Michael Ellison on 9/24/14.
+//  Copyright (c) 2014 MichaelWEllison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
